dcc70bc660be473b914b1976c4126cf8eb0186cf
Katie Beale <157204203+katiebealie@users.noreply.github.com>
2024-02-21 18:58:11 -0800

    second submission

--- python-project/PyBank/main.py
+++ python-project/PyBank/main.py
@@ -1,28 +1,39 @@
  # pybank script
    
+#import the right libraries
+
 import os
 import csv 
+
+#access the data source files and point to the file to be written on
 
 budget_data = os.path.join("Resources","budget_data.csv") 
 output_file = "Analysis.txt"
 
+#define initial variables for the loop
 Total_Months = []
-Net_Total = 0
+Total = 0
 Average_Changes = []
 Max_Increase = (0," ")
 Max_Decrease = (0, " ")
 
+#begin the loop
+
 with open(budget_data, newline="") as csvfile:
     budget_reader = csv.reader(csvfile, delimiter = ",")
-    next(budget_reader, None)
+    next(budget_reader, None) #skip the first row
     
     previous_row = next(budget_reader)     
     previous_value = int(previous_row[1])  
-    
+
+    budget_reader = csv.reader(csvfile, delimiter = ",") #reset the loop
+    Total_Months.append(previous_row)
+    Total += int(previous_row[1])
+    #next(budget_reader1, None)
     for row in budget_reader:
-       
+        
         Total_Months.append(row[0])
-        Net_Total += int(row[1])
+        Total += int(row[1])
         
         current_date = row[0]
         current_value = int(row[1])
@@ -38,22 +49,23 @@ with open(budget_data, newline="") as csvfile:
             
         previous_value = current_value
         
+#write the results in the analysis text file
 
 with open(output_file, "w") as txtfile:
     txtfile.write("Financial Analysis\n")
     txtfile.write("---------------------------\n")
-    txtfile.write(f"Total Months: {len(Total_Months)+1}\n")
-    txtfile.write(f"Net Total: ${Net_Total}\n")
+    txtfile.write(f"Total Months: {len(Total_Months)}\n")
+    txtfile.write(f"Total: ${Total}\n")
     txtfile.write(f"Average Change: ${sum(Average_Changes) / len(Average_Changes):.2f}\n")
     txtfile.write(f"Greatest Increase in Profits: {Max_Increase[1]} (${Max_Increase[0]})\n")
     txtfile.write(f"Greatest Decrease in Profits: {Max_Decrease[1]} (${Max_Decrease[0]})\n")
 
-
+#present the results in the terminal
 
 print("Financial Analysis")
 print("---------------------------")
-print(f"Total Months: {len(Total_Months) +1}")
-print(f"Net Total: ${Net_Total}")
+print(f"Total Months: {len(Total_Months)}")
+print(f"Total: ${Total}")
 print(f"Average Change: ${sum(Average_Changes) / len(Average_Changes):.2f}")
 print(f"Greatest Increase in Profits: {Max_Increase[1]} (${Max_Increase[0]})")
 print(f"Greatest Decrease in Profits: {Max_Decrease[1]} (${Max_Decrease[0]})")
--- python-project/PyPoll/main.py
+++ python-project/PyPoll/main.py
@@ -1,20 +1,23 @@
 #pypoll script
 
+#import the right libraries
 import os
 import csv
 from collections import Counter
+
+#access the source data file
 
 election_data = os.path.join("Resources", "election_data.csv")
 output_file = os.path.join("Analysis", "Analysis.txt")
 
 
-
+#loop through the data and count each vote
 
 with open(election_data, newline = "") as csvfile:
 
     election_reader = csv.reader(csvfile, delimiter=",")
         
-    next(election_reader, None)
+    next(election_reader, None) #make sure to skip the header row
 
     data = [row[2] for row in election_reader]
     counts = Counter(data)
@@ -22,27 +25,40 @@ with open(election_data, newline = "") as csvfile:
     Winner = [candidate for candidate, votes in counts.items() if votes == Max_Votes][0]
 
     listlength = counts.total()
-      
+
+#name the headers as variables for ease      
+
+Stockham = round(counts['Charles Casper Stockham']/listlength * 100, 3) #percentage of votes
+DeGette = round(counts['Diana DeGette']/listlength * 100, 3) #percentage of votes
+Doane = round(counts['Raymon Anthony Doane']/listlength * 100,  3) #percentage of votes
+Stockham_votes = counts['Charles Casper Stockham'] #individual vote count
+DeGette_Votes = counts['Diana DeGette'] #individual vote count
+Doane_votes = counts['Raymon Anthony Doane'] #individual vote count
+Total_Votes = listlength #count of all votes submitted
+
+#write the results in the text file
 
 with open(output_file, "w") as txtfile:
     txtfile.write("Election Results\n")
     txtfile.write("---------------------------\n")
     txtfile.write(f"Total Votes: {listlength}\n")
     txtfile.write("---------------------------\n")
-    txtfile.write(f"Charles Casper Stockham: {round(counts['Charles Casper Stockham']/listlength * 100, 3)}% ({counts['Charles Casper Stockham']})\n")
-    txtfile.write(f"Diana DeGette: {round(counts['Diana DeGette']/listlength * 100, 3)}% ({counts['Diana DeGette']})\n")
-    txtfile.write(f"Raymon Anthony Doane: {round(counts['Raymon Anthony Doane']/listlength * 100,  3)}% ({counts['Raymon Anthony Doane']})\n")
+    txtfile.write(f"Charles Casper Stockham: {Stockham}% ({Stockham_votes})\n")
+    txtfile.write(f"Diana DeGette: {DeGette}% ({DeGette_Votes})\n")
+    txtfile.write(f"Raymon Anthony Doane: {Doane}% ({Doane_votes})\n")
     txtfile.write("---------------------------\n")
     txtfile.write(f"Winner: {Winner} \n")
     txtfile.write("---------------------------\n")
+
+#present the results in the terminal as well
 
 print("Election Results")
 print("---------------------------")
 print(f"Total Votes: {listlength}")
 print("---------------------------")
-print(f"Charles Casper Stockham: {round(counts['Charles Casper Stockham']/listlength * 100, 3)}% ({counts['Charles Casper Stockham']})")
-print(f"Diana DeGette: {round(counts['Diana DeGette']/listlength * 100, 3)}% ({counts['Diana DeGette']})")
-print(f"Raymon Anthony Doane: {round(counts['Raymon Anthony Doane']/listlength * 100,  3)}% ({counts['Raymon Anthony Doane']})")
+print(f"Charles Casper Stockham: {Stockham}% ({Stockham_votes})")
+print(f"Diana DeGette: {DeGette}% ({DeGette_Votes})")
+print(f"Raymon Anthony Doane: {Doane}% ({Doane_votes})")
 print("---------------------------")
 print(f"Winner: {Winner} ")
 print("---------------------------")
